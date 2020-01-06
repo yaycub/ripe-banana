894bfa1f516524936fec1bfdf352448dc393c8a3
Jacob Harrington <jharrington4@gmail.com>
2020-01-06 15:10:36 -0800

    studio get by id route is completed and tests passing

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Studio = require('../lib/models/Studio');
+const Film = require('../lib/models/Film');
+const Actor = require('../lib/models/Actor');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -29,6 +31,30 @@ describe('app routes', () => {
       .get('/api/v1/studios')
       .then(res => {
         expect(res.body).toMatchObject(JSON.parse(JSON.stringify(studios)));
+      });
+  });
+
+  it('can get a studio by id', async() => {
+    const actor = await Actor.create({ name: 'Jacob' });
+    const studio = await Studio.create({ name: 'MGM', address: { city: 'LA', state: 'California', country: 'USA' } });
+    const film = await Film.create({
+      title: 'A film',
+      studio: studio._id,
+      released: 2020,
+      cast: [
+        { actor: actor._id } 
+      ]
+    });
+
+    return request(app)
+      .get(`/api/v1/studios/${studio._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: studio._id.toString(),
+          name: studio.name,
+          address: studio.address,
+          films: [{ _id: film._id.toString(), title: film.title }]
+        });
       });
   });
 });
